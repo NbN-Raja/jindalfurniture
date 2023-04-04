d261afa9f4ad8b197cae7040757ba98202946e99
NbN-Raja <xetrinbn66@gmail.com>
2023-04-05 03:29:52 +0545

    Add: Vercell version

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose= require('mongoose')
 const port = 3000
 
+const connection =mongoose.connect(`mongodb+srv://xetrinbn66:Y3vsCajF3saO2wNO
+@cluster0.sha62xh.mongodb.net/test`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
 app.get('/', (req, res) => res.send('Hello World! Jindal Furniture '))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
