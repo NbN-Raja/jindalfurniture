ae18e7637e9d3e2c2a513be521d2239971475d9b
NbN-Raja <xetrinbn66@gmail.com>
2023-04-05 23:41:10 +0545

    Add: mongo onlinr

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ require('./app/routes/products.routes')(app)
 
 const connection = mongoose
   .connect(
-    `mongodb://localhost:27017/test`,
+    `mongodb+srv://xetrinbn66:Y3vsCajF3saO2wNO
+    @cluster0.sha62xh.mongodb.net/test`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
